9afd4587117999304756805c9c503dd923334ba4
Your Name <you@example.com>
2022-12-01 21:26:28 +0100

    second

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# tool_start_params
-tool_start_params
+
+Programm start und eine Parameterübergabe mit Hilfe einer console (cmd.exe etc.):
+
+Ohne Zusatzparametern:
+
+start rrzz://
+start "" "rrzz://"
+
+
+
+Mit Zisatzparametern:
+
+C:\Users\Administrator>start "" "some://url?with=special&chars=:->"
+C:\Users\Administrator>start "" "rrzz://url?with=special&chars=:->"
+
+C:\Users\Administrator>start rrzz://folder23/gruppe12
+C:\Users\Administrator>start "" "rrzz://url?folder=folder23&gruppe=12"
+
+
+Programm start und eine Parameterübergabe mit Hilfe einer console direkt ohne Protokol (cmd.exe etc.):
+
+Ohne Zusatzparametern:
+
+PS C:\Users\Administrator\Desktop> .\params.exe
+
+
+
+Mit Zisatzparametern:
+
+PS C:\Users\Administrator\Desktop> .\params.exe testParam01 testParam02 testParam03 fgdf fdg dfg3454
+PS C:\Users\Administrator\Desktop> .\params.exe testParam01 testParam02 testParam03
+
+
+C:\java_projects\serpinskyTriangle01\serpinskyTriangle\target> java -jar .\serpinskyTriangle-1.0-SNAPSHOT.jar 1 mkmkmk 67
+
+http://java.com/download
+
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,47 +7,57 @@ import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
 /**
- * Hello world!
+ * A tool for show Java-Arguments.
  */
 public class App {
     public static void main(String[] args) {
 
         // JOptionPane.showMessageDialog(null, "A tool for java Argumente.");
 
-        if (args == null) System.out.println("The args == null.");
-
-        if (args != null) System.out.println("The args != null.");
-
-        if (args.length == 0) {
-            System.out.println("The args are not null.");
-        }
-        else
-        {
-            System.out.println("The args are null.");
-        }
-
-        if (args.length > 0) {
-            System.out.println("The args > 0.");
-        }
-
-        System.out.println("args.count(): " + Arrays.stream(args).count());
-
-        for (String s : args) {
-            System.out.println("s: " + s);
-        }
+//        if (args == null) System.out.println("The args == null.");
+//
+//        if (args != null) System.out.println("The args != null.");
+//
+//        if (args.length == 0) {
+//            System.out.println("The args args.length == 0.");
+//        } else {
+//            System.out.println("The args are null.");
+//        }
+//
+//        if (args.length > 0) {
+//            System.out.println("The args > 0.");
+//        }
+//
+//        System.out.println("args.count(): " + Arrays.stream(args).count());
+//
+//        for (String s : args) {
+//            System.out.println("s: " + s);
+//        }
 
         // Erzeugung eines neuen Dialoges
         JDialog meinJDialog = new JDialog();
-        meinJDialog.setTitle("A tool for java Argumente.");
-        meinJDialog.setSize(450, 300);
+        meinJDialog.setTitle("A tool for show Java-Arguments.");
+        meinJDialog.setSize(800, 200);
         // JPanel wird erzeugt
         JPanel panel = new JPanel();
         // Unser JPanel erhält einen langen Schriftzug
 
-        panel.add(new JLabel("Argumente:"));
+        panel.add(new JLabel("Shown arguments: "));
+
+        if (args == null) panel.add(new JLabel("The args == null "));
+
+        if (args != null) panel.add(new JLabel("The args != null "));
+
+        if (args.length == 0) {
+            panel.add(new JLabel("The args args.length == 0 "));
+        } else panel.add(new JLabel("args are != 0 "));
+
+        if (args.length > 0) panel.add(new JLabel("The args > 0 "));
+
+        panel.add(new JLabel(" args.count(): " + Arrays.stream(args).count() + " "));
 
         for (String s : args) {
-            panel.add(new JLabel("arg: " + s + ", "));
+            panel.add(new JLabel(" arg: " + s + " "));
         }
 
         // JScrollPane wird erzeugt; dabei wird über den
